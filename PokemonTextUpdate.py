9a9fb44e2ef1e1569c33858306b289aa6e95b961
Melody Timber <raceres@gmail.com>
2020-01-09 17:03:33 -0700

    Add files via upload
    Added all six slots, basic input to test visuals with.

--- PokemonTextUpdate.py
+++ PokemonTextUpdate.py
@@ -1,4 +1,74 @@
 # Text Writer
 # Melody McGee, 2020
 # Designed to update pokemon information in a livestream via text
-# 
+ 
+# --TODO--
+# Track pokemon catch order?
+# Add image copy/rename
+# Add revive counter per pokemon
+# Maintain database of pokemon caught
+
+# Pokemon type and name variables
+p1 = input("Pokemon in slot 1? ")
+p2 = "test"
+p3 = "test"
+p4 = "test"
+p5 = "test"
+p6 = "test"
+n1 = input(p1 + " nickname? ")
+n2 = "test"
+n3 = "test"
+n4 = "test"
+n5 = "test"
+n6 = "test"
+
+
+
+
+pokemon1 = open("pokemon1.txt", "w")
+pokemon1.write(p1)
+pokemon1.close()
+
+nickname1 = open("nickname1.txt", "w")
+nickname1.write(n1)
+nickname1.close()
+
+pokemon2 = open("pokemon2.txt", "w")
+pokemon2.write(p2)
+pokemon2.close()
+
+nickname2 = open("nickname2.txt", "w")
+nickname2.write(n2)
+nickname2.close()
+
+pokemon3 = open("pokemon3.txt", "w")
+pokemon3.write(p3)
+pokemon3.close()
+
+nickname3 = open("nickname3.txt", "w")
+nickname3.write(n3)
+nickname3.close()
+
+pokemon4 = open("pokemon4.txt", "w")
+pokemon4.write(p4)
+pokemon4.close()
+
+nickname4 = open("nickname4.txt", "w")
+nickname4.write(n4)
+nickname4.close()
+
+pokemon5 = open("pokemon5.txt", "w")
+pokemon5.write(p5)
+pokemon5.close()
+
+nickname5 = open("nickname5.txt", "w")
+nickname5.write(n5)
+nickname5.close()
+
+pokemon6 = open("pokemon6.txt", "w")
+pokemon6.write(p6)
+pokemon6.close()
+
+nickname6 = open("nickname6.txt", "w")
+nickname6.write(n6)
+nickname6.close()
